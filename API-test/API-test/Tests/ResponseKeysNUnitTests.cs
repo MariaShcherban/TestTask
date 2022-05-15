746c5a27348a8a8e073e1a4f0867da1d1c09d9ca
MariaShcherban <manya112007@gmail.com>
2022-05-15 16:41:22 +0300

    Added test to verify json structure

---
+++ API-test/API-test/Tests/ResponseKeysNUnitTests.cs
@@ -0,0 +1,38 @@
+﻿using API_test.ApiResponseModels;
+using API_test.Helpers;
+using API_test.RequestSpecs;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace API_test.Tests
+{
+    public class ResponseKeysNUnitTests
+    {
+        private RestHelper restHelper;
+
+        [SetUp]
+        public void Setup()
+        {
+            restHelper = new RestHelper(RequestSpec.BackendUrl);
+        }
+
+        [Test]
+        public void KeysInJsonShouldBeCorrect()
+        {
+            var response = restHelper.GetQuery(RequestSpec.Query);
+
+            var total = response.Content.ToObject<TotalDto>();
+            Assert.IsNotNull(total.Total);
+
+            var items = response.Content.SelectToken("items").ToObject<List<ItemDto>>();
+            foreach (ItemDto item in items)
+            {
+                Assert.IsNotNull(item.Code);
+                Assert.IsNotNull(item.Id);
+                Assert.IsNotNull(item.Name);
+                Assert.IsNotNull(item.Country.Code);
+                Assert.IsNotNull(item.Country.Name);
+            }    
+        }
+    }
+}
